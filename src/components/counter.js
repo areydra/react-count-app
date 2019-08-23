d65359e3f9306d822883df7d52a20ae50b205ff6
Areydra <areydra@gmail.com>
2019-08-23 11:34:29 +0700

    create components counter

---
+++ src/components/counter.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    state = {
+        count : 0
+    }
+
+    handleIncrement = product => {
+        console.log(product) //product(bisa mengubah count sesuai nama product/id product)
+        this.setState({ count : this.state.count + 1 }) //gunakan setState untuk melakukan operator matematika setelah eventclick, karena secara default react blm support
+        //this.setState() asalnya dari class Component di module react
+    }
+
+    render(){
+        return (
+            <div>
+                <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
+                <button onClick={ () => this.handleIncrement('Gerry Chocolate') } className="btn btn-secondary btn-sm">
+                    Increment
+                </button>
+            </div>
+        )
+    }
+
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-"
+        classes += this.state.count === 0 ? "warning" : "primary" //jika state count value 0 maka tambahkan warning jika tidak tambah primary jadi badge m-2 badge-warning or badge m-2 badge-primary
+        return classes
+    }
+
+    formatCount(){
+        const { count } = this.state //artnya mengambil key/method count yg ada di object state
+        return count === 0 ? "Zero" : count 
+    }
+}
+
+export default Counter //menjadikan class Counter default
+
+// NOTE : untuk memanggil function dan variabel javascript didalam tag html, gunakan : kurung kurawa seperti diatas {}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Counter from './components/counter' //penamaan class harus huruf capital didepannya. contoh ini mengimport class Counter
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css' //import file bootstrap.css agar bisa digunakan secara global
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root')); //masukan component counter kedalam file index.html di public dengan id tag root
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
